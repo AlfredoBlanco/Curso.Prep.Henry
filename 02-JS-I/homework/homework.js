22fb81f30131a2232ef358e245cb788580e31979
Alfredo Blanco <alfre.blanco@icloud.com>
2021-11-01 17:46:22 -0300

    First Homework

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -248,7 +248,7 @@ function esVocal(letra){
   if (letra.length == 1){
     letra.toLowerCase();
     if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'){
-        return 'Es vocal';
+        return 'Es vocal' ;
     }
     return 'Dato incorrecto';
   }
